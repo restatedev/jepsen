9cd5180887a66ce383e73173a3f0120392777c9a
igalshilman <igal@restate.dev>
2023-05-06 19:54:59 +0200

    Add restate provisioning

--- src/jepsen/restate.clj
+++ src/jepsen/restate.clj
@@ -1,14 +1,49 @@
 (ns jepsen.restate
-  (:require [jepsen.cli :as cli]
-            [jepsen.tests :as tests]))
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as str]
+            [jepsen [cli :as cli]
+             [control :as c]
+             [db :as db]
+             [tests :as tests]]
+            [jepsen.control.util :as cu]
+            [jepsen.os.debian :as debian]))
 
+
+(def dir     "/opt/restate")
+(def binary "restate")
+(def logfile (str dir "/restate.log"))
+(def pidfile (str dir "/restate.pid"))
+
+ 
+(defn db
+  "Restate for a particular version."
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info node "installing restate" version)
+      (c/su
+        (c/exec :systemctl :restart :docker)
+        (c/exec :docker :pull "ghcr.io/restatedev/restate:latest")
+        (c/exec :docker :run :-itd :--rm :-p "8081:8081" :-p "9090:9090" "ghcr.io/restatedev/restate:latest"))
+      )
+
+    (teardown! [_ test node]
+      (info node "tearing down restate")
+      (c/su
+        (c/exec :docker :ps :-aq :| :xargs :docker :stop :|| true))
+      )
+    ))
+           
 
 (defn restate-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
   [opts]
   (merge tests/noop-test
-         {:pure-generators true}
+         {:name "restate"
+          :os   debian/os
+          :db   (db "v3.1.5")
+          :pure-generators true}
          opts))
 
 (defn -main
