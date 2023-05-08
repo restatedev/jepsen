e128afd28e6cb2eeed59ee3be529e953f5cfeb8c
igalshilman <igal@restate.dev>
2023-05-08 20:18:06 +0200

    Delete unused dist cluster

--- src/jepsen/restate_cluster.clj
+++ src/jepsen/restate_cluster.clj
@@ -33,22 +33,3 @@
       (c/su
         (c/exec :docker :ps :-aq :| :xargs :docker :stop :|| true))
       )))
-
-(defn make-distributed-cluster
-  "Restate for a particular version."
-  [version]
-  (reify jepsen.db/DB
-    (setup! [_ _ node]
-      (info node "installing restate" version)
-      (c/su
-        (c/exec :systemctl :restart :docker)
-        (c/exec :docker :pull "ghcr.io/restatedev/restate:latest")
-        (c/exec :docker :run :-itd :--rm :-p "8081:8081" :-p "9090:9090" "ghcr.io/restatedev/restate:latest"))
-      )
-
-    (teardown! [_ _ node]
-      (info node "tearing down restate")
-      (c/su
-        (c/exec :docker :ps :-aq :| :xargs :docker :stop :|| true))
-      )))
-
