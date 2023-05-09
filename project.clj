454148b334766ebe90982a3aa2e5fd3cfe3c44be
igalshilman <igal@restate.dev>
2023-05-09 16:29:34 +0200

    Add JVM opts and use a newer version

--- project.clj
+++ project.clj
@@ -4,9 +4,16 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :main jepsen.restate
+  :aot :all
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [jepsen "0.3.1"]
+                 [jepsen "0.3.2"]
                  [verschlimmbesserung "0.1.3"]
                  [org.clojure/data.json "2.4.0"]
                  ]
-  :repl-options {:init-ns jepsen.restate})
+  :repl-options {:init-ns jepsen.restate}
+
+  :jvm-opts ["-Xmx8g"
+             "-server"
+             "-XX:+UseZGC"
+             "-XX:-OmitStackTraceInFastThrow"]
+  )
