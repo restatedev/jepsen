6c10ceb63d7bd04887c0ceb637f84d58adc669ac
Pavel Tcholakov <pavel@restate.dev>
2025-01-29 22:53:41 +0200

    Fix key perms

--- aws/get-node-info.sh
+++ aws/get-node-info.sh
@@ -7,6 +7,7 @@ STACK_NAME=$(jq -r 'keys | .[0]' cdk-outputs.json)
 aws ssm get-parameters \
     --name "$(jq -r --arg stack_name "${STACK_NAME}" '.[$stack_name].KeyArn' cdk-outputs.json)" \
     --with-decryption --query "Parameters[0].Value" --output text > private-key.pem
+chmod 600 private-key.pem
 
 jq -r --arg stack_name "${STACK_NAME}" \
     '.[$stack_name] | to_entries[] | select(.key | test("^Node")) | .value' cdk-outputs.json \
